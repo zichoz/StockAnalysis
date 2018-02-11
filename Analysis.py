41711738e7b547ef7d5a30cd8276bc6ed7a8e97e
zichoz <35935836+zichoz@users.noreply.github.com>
2018-02-11 21:48:21 +0100

    Add files via upload

--- Analysis.py
+++ Analysis.py
@@ -1,24 +1,27 @@
 import ondemand
 from Stock import Stock
-
+from Industry import Industry
 
 od = ondemand.OnDemandClient(api_key='5dbee5d0272ae52936a8cabbdcef6ea6', end_point='https://marketdata.websol.barchart.com/')
-
+#The stocks i use are just random picked from the stock exchange
 quotes = od.quote('DDD,MMM,WBAI,WUBA,EGHT,AHC,ATEN,AAC,AIR,AAN,ABB,ABT,ABBV,ANF,GCH,ACP,JEQ,SGF,ABM,AKR,ACN,ACCO,ATV,ATU,AYI,GOLF,ADX,PEO,AGRO,ADNT,ATGE,AAP,ADSW,WMS,ASX,ASIX,AAV,AVK,AGC,LCM,ACM,ANW,AEB,AED,AEG,AEH,AEK,AER,HIVE,AJRD,AET,AMG,AFL,MITT,MITT^A,MITT^B,AGCO,A,AEM,ADC,AGU,AL,APD,AYR,AKS,ALP^Q,ALG,AGI,ALK,AIN,ALB,AA,ALEX,ALX,ARE,ARE^D,AQN,BABA,Y,ATI,ALLE,AGN,AGN^A,ALE,AKP,ADS,AFB,AOI,AWF,AB,LNT,CBH,NCV,NCZ,ACV,NIE,NFJ,ALSN,ALL,ALL^A,ALL^B,ALL^C,ALL^D,ALL^E,ALL^F,ALLY,ALLY^A,ALDW,AGD,AWP,AOD,AYX,ATUS,RESI,MO,ACH,AMBR,ABEV,AMC,AEE,AMRC,AMOV,AMX,AAT,AXL,ACC,AEO,AEP,AEL,AXP,AFG,AFGE,AFGH,AMH,AMH^C,AMH^D,AMH^E,AMH^F,AMH^G,AIG,AIG.WS,AMID,ARL,ARA,AWR', 'dividendRateAnnual')['results']
 
 
 resp = od.get('getQuote', symbols='AAPL,EXC', fields='bid,ask')
 
 stocks = []
-
+industries = []
 
 for q in quotes: 	#Creates a object and saves it in the list "stocks"
 	stockObj = Stock(q['symbol'], q['name'], q['dayCode'], q['serverTimestamp'], q['mode'], q['lastPrice'], q['tradeTimestamp'], q['netChange'], q['percentChange'], q['unitCode'], q['open'], q['high'], q['low'], q['close'], q['dividendRateAnnual'])
 	counter =+ 1
 	stocks.append(stockObj)
 
+#This reades the file to find the industry information
 
-#print(stocks[0].dividendRateAnnual)
+
+
+
 
 def findStockData(): #method for finding stockdata for the user 
 	nameSearch = input("What stock do you want to check out?")
@@ -47,33 +50,3 @@ def findDividendStocks(): #functions from finding dividendStocks from iterating 
 				noDividendStocks.append(q)
 				#adds a stock that does not pay dividend in a list.
 
-findDividendStocks()
-print (len(noDividendStocks))
-
-print (len(dividendStocks))
-print (dividendStocks[1].name)
-#print(len(aksjenavn))
-#getStockNames()
-#print(len(aksjenavn))
-
-
-
-
-
-
-
-#resp = od.history('ATVI', 'minutes', maxRecords=50, interval=1)
-#od = ondemand.OnDemandClient(api_key='5dbee5d0272ae52936a8cabbdcef6ea6')
-#I use Barchart.com's API for intra day stock data
-
-
-#od = ondemand.OnDemandClient(api_key='5dbee5d0272ae52936a8cabbdcef6ea6')
-#I use Barchart.com's API for intra day stock data
-#odDivdend = ondemand.getDividendData(api_key='5dbee5d0272ae52936a8cabbdcef6ea6', end_point='https://marketdata.websol.barchart.com/')
-# get quote data for Apple and Microsoft
-
-#print (resp)
-#for r in resp: 
-#	print('Symbol: %s, Volume: %s' % (r['symbol'], r['volume']))
-#print (od.quote('AMZN,MSFT'))
-#divdendPay = od.divdend('AAPL,MSFT')
---
+++ Industry.py
@@ -0,0 +1,14 @@
+class Industry:
+	def __init__(self, industryName, amountOfFirms, beta, debtEquityratio, effectiveTaxRate, unleveredBeta, cashFirmValue, unleveredBetaCorrectedForCash, hiLoRisk, standardDeviationOfEquity, standardDeviationInOperatingIncomeTenYears):
+		self.industryName = industryName
+		self.amountOfFirms = amountOfFirms
+		self.beta = beta
+		self.debtEquityratio = debtEquityratio
+		self.effectiveTaxRate = effectiveTaxRate
+		self.unleveredBeta = unleveredBeta
+		self.cashFirmValue = cashFirmValue
+		self.unleveredBetaCorrectedForCash = unleveredBetaCorrectedForCash
+		self.hiLoRisk = hiLoRisk
+		self.standardDeviationOfEquity= standardDeviationOfEquity
+		self.standardDeviationInOperatingIncomeTenYears = standardDeviationInOperatingIncomeTenYears
+
