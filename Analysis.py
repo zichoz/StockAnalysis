950cd75a61cca67df7dda19d68b45097dbf1c18d
zichoz <35935836+zichoz@users.noreply.github.com>
2018-02-19 10:34:18 +0100

    Add files via upload

--- Analysis.py
+++ Analysis.py
@@ -13,23 +13,6 @@ resp = od.get('getQuote', symbols='AAPL,EXC', fields='bid,ask')
 stocks = []
 industries = []
 nyseStockSymbols = []
-
-"""
-for q in quotes: 	#Creates a object and saves it in the list "stocks"
-	stockObj = Stock(q['symbol'], q['name'], q['dayCode'], q['serverTimestamp'], q['mode'], q['lastPrice'], q['tradeTimestamp'], q['netChange'], q['percentChange'], q['unitCode'], q['open'], q['high'], q['low'], q['close'], q['dividendRateAnnual'])
-	counter =+ 1
-	#print(stockObj.dividendRateAnnual)
-	stocks.append(stockObj)
-"""
-#This reades the file to find the industry information
-
-#industryObj = Industry()
-						#reads the industry information from industries.txt and creates and object for each industry and puts it in the list: industries[]
-
-
-
-
-
 								#Reads from the NYSE STOCK FILE
 
 											#
@@ -37,27 +20,67 @@ for line in nyseSymbolsCsv:
 	currentSymbol = line.split(",")				#Function for making a list with all the stock symbols from the NYSE.CSV
 	
 	for i in range(len(currentSymbol)):
-			nyseStockSymbols.append(currentSymbol[i]+",")
+			nyseStockSymbols.append(currentSymbol[i])
 
-someStocks = (nyseStockSymbols[622]+ nyseStockSymbols[352])		
-print (someStocks)
+
+	#will have to make a string which includes all the 100 tickers i want to check out
+
 nyseStockSymbols[3139] = "ZYME"
 
-
-def readTodaysStockDataFromNyse():         #Working to make this a function where i can request 100 tickets for each request i can make, as i only can make 2500 a day
-
-	dailyQuotes = od.quote(someStocks, 'dividendRateAnnual')['results']
+def readTodaysStockData(stocksToAnalyze):         #Working to make this a function where i can request 100 tickets for each request i can make, as i only can make 2500 a day
+	
+	dailyQuotes = od.quote(stocksToAnalyze, 'dividendRateAnnual')['results']
 	for q in dailyQuotes: 
 		stockObj = Stock(q['symbol'], q['name'], q['dayCode'], q['serverTimestamp'], q['mode'], q['lastPrice'], q['tradeTimestamp'], q['netChange'], q['percentChange'], q['unitCode'], q['open'], q['high'], q['low'], q['close'], q['dividendRateAnnual'])
-		print (stockObj.name)
+		#print (stockObj.name + "   " + str(stockObj.lastPrice))
 		stocks.append(stockObj)
 
 
 
 
+
+print (((int((int(len(nyseStockSymbols)))/100)))+1)
+def gatherInfoAboutNyse(): 
+	counterX = 0
+	counterY = 99
+	#Makes the list And sets the first item as the 100 symbols
+	for i in range(0, (((int((int(len(nyseStockSymbols)))/100)))+1)): 
+		print (counterY)            #takes 100 symbols from the lists and converts them to one string and then runs the function with them as an argument
+		if (counterY > len(nyseStockSymbols)):
+
+			counterY = len(nyseStockSymbols)
+			print(" sant" + str(counterY))
+			listWithHundredtest= str([nyseStockSymbols[x] for x in range(counterX, counterY)])
+			listWithHundredtestTwo = listWithHundredtest.replace("'", "")            # Had to make this to convert the list to one string with all the items combined
+			listWithHundredtestThree = listWithHundredtestTwo.replace(" ", "")
+			listwithHundredtestFour = listWithHundredtestThree.replace("[", "'")
+			someStocks = listwithHundredtestFour.replace("]", "'")
+			#print (someStocks + " derp")
+			counterX = counterX + 100
+			counterY = counterY + 100
+			readTodaysStockData(someStocks)
+			menu()
+		else : 
+			print ("usant")
+			listWithHundredtest= str([nyseStockSymbols[x] for x in range(counterX, counterY)])
+			listWithHundredtestTwo = listWithHundredtest.replace("'", "")            # Had to make this to convert the list to one string with all the items combined
+			listWithHundredtestThree = listWithHundredtestTwo.replace(" ", "")
+			listwithHundredtestFour = listWithHundredtestThree.replace("[", "'")
+			someStocks = listwithHundredtestFour.replace("]", "'")
+			#print (someStocks + " derp")
+			counterX = counterX + 100
+			counterY = counterY + 100
+			readTodaysStockData(someStocks)
+
+
+
+
+
+
+#readTodaysStockDataFromNyse(someStocks)
 #print (len(stockObj))
 
-
+#readTodaysStockDataFromNyse(str(someStocks))
 
 
 
@@ -81,7 +104,7 @@ def findStockData(): #method for finding stockdata for the user
 	for q in stocks:
 		if nameSearch in q.name:
 			print("Name :" + q.name + "   Symbol : " + q.symbol +  "   Last Price :" + str(q.lastPrice))
-		
+	menu()	
 	
 text_file = open("Output.txt", "w")
 for q in stocks:
@@ -95,14 +118,39 @@ stockYield = 0.00
 def findDividendStocks(): #functions from finding dividendStocks from iterating throug the Stocks list 
 	noDividendStock = "0"
 	for q in stocks:
-			if (q.dividendRateAnnual != "0"): 
-				if (q.dividendRateAnnual != None): 
-					dividendStocks.append(q)
-					stockYield = float(q.dividendRateAnnual)/float(q.lastPrice)
+		if (q.dividendRateAnnual != "0"): 
+			if (q.dividendRateAnnual != None): 
+				dividendStocks.append(q)
+
+				stockYield = float(q.dividendRateAnnual)/float(q.lastPrice)
+				print (q.name + "       " +  str(stockYield))
 					#print (str(q.name) + "     " + str(stockYield))
 					#Checks if the returned variable is not 0 and not None then adds them to the list
-			else: 
-				noDividendStocks.append(q)
-				#adds a stock that does not pay dividend in a list.
-
+		else: 
+			noDividendStocks.append(q)
+			#adds a stock that does not pay dividend in a list.
+	menu()
 #readTodaysStockDataFromNyse()
+
+
+def menu(): 	
+	print ("option 1 : load new stockData from NYSE")
+	print ("Option 2 : find information about one Stock")
+	print ("Option 3 : Find Dividendstocks")
+	print ("Option 4 : Quit")
+
+	keyword = int(input("Type a number between 1 - 4 :      "))
+
+	if (keyword == 1): 
+		gatherInfoAboutNyse()
+	elif (keyword == 2):
+		findStockData()
+	elif (keyword == 3):
+		print ("test")
+		findDividendStocks()
+	elif (keyword == 4):
+		print ("Quitting!")
+	else: 
+		menu()
+
+menu()
